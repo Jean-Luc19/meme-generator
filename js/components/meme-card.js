e0bf4e0edebe9e54105b70e1fa9a214d47a7b806
Aaron Gottlieb <aticus19@gmail.com>
2017-03-08 10:28:12 -0700

    adds input handlers to meme-card

--- js/components/meme-card.js
+++ js/components/meme-card.js
@@ -1,16 +1,40 @@
 import React from 'react';
 
-export default function MemeCard(props) {
-  return (
-    <div className="meme-card">
-      <label></label>
-      <input></input>
-      <label></label>
-      <input></input>
-      <img src="http://hung-meme-maker.herokuapp.com/system/photos/images/000/000/001/thumb/AintNobody.jpg?1374553487"></img>
-      <p>coding from scratch</p>
-      <p>Nobodys got time for that!</p>
-      <button>Submit</button>
-    </div>
-  );
+export default class MemeCard extends React.Component {
+  constructor() {
+      super();
+      this.state = {
+          topText: '',
+          bottomText: ''
+      }
+
+      this.topChangeHandler = this.topChangeHandler.bind(this);
+      this.bottomChangeHandler = this.bottomChangeHandler.bind(this);
+
+  }
+  topChangeHandler (e) {
+      this.setState({topText: e.target.value})
+      console.log(this.state.topText, e.target.value);
+  }
+
+  bottomChangeHandler (e) {
+      this.setState({bottomText: e.target.value})
+      console.log("hello");
+  }
+  render () {
+      return (
+        <div className="meme-card">
+          <label></label>
+          <input type="text" onChange={this.topChangeHandler}/>
+          <label></label>
+          <input type="text" onChange={this.bottomChangeHandler}/>
+          <img src="http://hung-meme-maker.herokuapp.com/system/photos/images/000/000/001/thumb/AintNobody.jpg?1374553487"></img>
+          <p>{this.state.topText}</p>
+          <p>{this.state.bottomText}</p>
+          <button>Submit</button>
+        </div>
+      );
+
+  }
+
 }
