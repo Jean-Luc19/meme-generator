fcc4bd6784a2aaf73039eedef07d919feb85fc51
Aaron Gottlieb <aticus19@gmail.com>
2017-03-08 13:50:06 -0700

    hides edit area and displays it on click

--- js/components/meme-card.js
+++ js/components/meme-card.js
@@ -5,12 +5,13 @@ import Mememedia from './meme-media';
 
 export default function MemeCard(props) {
   return (
-    <div className="meme-card">
+    <div className={`meme-card ${props.isDisplaying ? '' : 'hidden'}`}>
       <h1>Meme Generator</h1>
       <Toptext topChangeHandler={props.topChangeHandler} topText={props.topText} />
       <Bottomtext bottomChangeHandler={props.bottomChangeHandler} bottomText={props.bottomText} />
       <Mememedia topText={props.topText} bottomText={props.bottomText} imgSrc={props.currentImg}/>
       <button>Submit</button>
+      <button onClick={() => props.displayEditArea(false)}>Cancel</button>
     </div>
   );
 }
--- js/components/meme-container.js
+++ js/components/meme-container.js
@@ -8,6 +8,7 @@ export default class MemeContainer extends React.Component {
     constructor() {
         super();
         this.state = {
+            editArea: false,
             topText: '',
             bottomText: '',
             imgSrc: "http://hung-meme-maker.herokuapp.com/system/photos/images/000/000/001/thumb/AintNobody.jpg?1374553487",
@@ -16,6 +17,7 @@ export default class MemeContainer extends React.Component {
 
         this.topChangeHandler = this.topChangeHandler.bind(this);
         this.bottomChangeHandler = this.bottomChangeHandler.bind(this);
+        this.displayEditArea = this.displayEditArea.bind(this);
 
     }
 
@@ -35,19 +37,34 @@ export default class MemeContainer extends React.Component {
       this.setState({imgSrc: img})
     }
 
+    displayEditArea(shouldDisplay){
+        if (!shouldDisplay) {
+            return this.setState({ editArea: false, topText: '', bottomText: '', imgSrc: '' });
+        }
+
+        this.setState({ editArea: true });
+    }
+
+    handleImageClick(e, img) {
+        this.cardBuilder(e, img);
+        this.displayEditArea(true);
+    }
+
 
   render() {
     return (
         <div className="image-list">
             <ul>
                 {this.state.images.map((img, index) =>
-                  <li><img src={img} onClick={e => this.cardBuilder(e, img)} key={index} /></li>)}
+                  <li><img src={img} onClick={e => this.handleImageClick(e, img)} key={index} /></li>)}
             </ul>
             <MemeCard topText={this.state.topText}
                       bottomText={this.state.bottomText}
                       topChangeHandler={this.topChangeHandler}
                       bottomChangeHandler={this.bottomChangeHandler}
                       currentImg={this.state.imgSrc}
+                      isDisplaying={this.state.editArea}
+                      displayEditArea={this.displayEditArea}
 
             />
         </div>
