2d22b3bbf667f6ecdb153514167b7cd219f43446
James Guthrie <jamesrguthrie@gmail.com>
2017-03-08 11:49:17 -0500

    added meme-card component and connected it to index.js

---
+++ js/components/meme-card.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function MemeCard(props) {
+  return (
+    <div className="meme-card">
+      <img src="http://hung-meme-maker.herokuapp.com/system/photos/images/000/000/001/thumb/AintNobody.jpg?1374553487"></img>
+      <p>coding from scratch</p>
+      <p>Nobodys got time for that!</p>
+    </div>
+  );
+}
--- js/index.js
+++ js/index.js
@@ -1,2 +1,8 @@
 require('babel-polyfill');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MemeCard from './components/meme-card'
 
+document.addEventListener('DOMContentLoaded', () =>
+  ReactDOM.render(<MemeCard />, document.getElementById('app'))
+);
